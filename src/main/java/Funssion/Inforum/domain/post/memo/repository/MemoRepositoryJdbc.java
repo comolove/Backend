6920bfa077699b0e9565810e1787885cb8dfef5e
comolove <66062558+comolove@users.noreply.github.com>
2023-08-17 21:49:06 +0900

    Update MemoRepositoryJdbc.java

--- src/main/java/Funssion/Inforum/domain/post/memo/repository/MemoRepositoryJdbc.java
+++ src/main/java/Funssion/Inforum/domain/post/memo/repository/MemoRepositoryJdbc.java
@@ -143,7 +143,7 @@ public class MemoRepositoryJdbc implements MemoRepository{
     public Memo updateContentInMemo(MemoSaveDto form, Long memoId) {
 
         String sql = "update memo.info " +
-                "set memo_title = ?, memo_description = ?, memo_text = ?::jsonb, memo_color = ?, updated_date = ?" +
+                "set memo_title = ?, memo_description = ?, memo_text = ?::jsonb, memo_color = ?, updated_date = ? " +
                 "where memo_id = ?";
 
         if (template.update(sql,
