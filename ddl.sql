9e4e03d41486db942e323d7a52848af1f22946ff
comolove <66062558+comolove@users.noreply.github.com>
2023-08-17 20:53:41 +0900

    Create ddl.sql

---
+++ ddl.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "member"."like"  (
+    id bigserial PRIMARY KEY,
+    user_id int8 NOT NULL,
+    post_type varchar NOT NULL,
+    post_id int8 NOT NULL,
+    created timestamp DEFAULT current_timestamp
+);
+
+select * from like;
+
+select * from mememr;
